c4326d5f5a1ee8376f0539d5f824619a82bdf9d2
kbengine <3603661@qq.com>
2016-11-24 13:00:32 +0800

    up

---
+++ Source/kbengine_ue4_demo/ue4_scripts/GameModeSelectAvatar.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "kbengine_ue4_demo.h"
+#include "GameModeSelectAvatar.h"
+
+
+
+
---
+++ Source/kbengine_ue4_demo/ue4_scripts/GameModeSelectAvatar.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GameModeSelectAvatar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KBENGINE_UE4_DEMO_API AGameModeSelectAvatar : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

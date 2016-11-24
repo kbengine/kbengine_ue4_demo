ea0415290b5e715a35488a42aa8d00bb48c72387
kbengine <3603661@qq.com>
2016-11-24 14:29:28 +0800

    up

---
+++ Source/kbengine_ue4_demo/ue4_scripts/GameModeWorld.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GameModeWorld.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KBENGINE_UE4_DEMO_API AGameModeWorld : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

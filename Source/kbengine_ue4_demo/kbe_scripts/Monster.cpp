729035da8b261d5829b5e0af2f0f0d68cb5b960d
kebiao <380000937@qq.com>
2018-02-07 16:43:28 +0800

    up

--- Source/kbengine_ue4_demo/kbe_scripts/Monster.cpp
+++ Source/kbengine_ue4_demo/kbe_scripts/Monster.cpp
@@ -5,6 +5,9 @@
 /*
 	每个实体都需要将自己注册，格式为
 	ENTITYDEF_CLASS_REGISTER(实体模块名，继承模块1，继承模块N，...)
+
+	当实体类没有继承任何def中的类时使用该宏, 如这种实体: class Monster : public Entity
+	ENTITYDEF_CLASS_NO_DEFPARENT_REGISTER
 */
 ENTITYDEF_CLASS_REGISTER(Monster, GameObject, NPCObject, Combat, Motion, State, Flags, Spell)
 

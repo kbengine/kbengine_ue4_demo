3c6030b99da815ddaf8ac74b8b2741a60aa49aa1
kebiao <380000937@qq.com>
2018-11-19 21:05:52 +0800

    up

--- Plugins/kbengine_ue4_plugins/Source/KBEnginePlugins/Engine/Entity.h
+++ Plugins/kbengine_ue4_plugins/Source/KBEnginePlugins/Engine/Entity.h
@@ -86,6 +86,12 @@ public:
 	}
 
 public:
+	void destroy()
+	{
+		detachComponents();
+		onDestroy();
+	}
+
 	virtual void onDestroy()
 	{
 	}
@@ -152,6 +158,16 @@ public:
 	}
 
 	virtual void callPropertysSetMethods()
+	{
+		// 动态生成
+	}
+
+	virtual void attachComponents()
+	{
+		// 动态生成
+	}
+
+	virtual void detachComponents()
 	{
 		// 动态生成
 	}
